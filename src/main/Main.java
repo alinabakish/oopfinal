47666408237c8eb5ac8bae9e0ebc196108e99929
alinabakish <94388390+alinabakish@users.noreply.github.com>
2022-01-15 02:25:25 +0600

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,28 @@
+package main;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application {
+
+    public static final String CURRENCY = "$";
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("../views/login.fxml")));
+        primaryStage.setTitle("Fruits Market");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
